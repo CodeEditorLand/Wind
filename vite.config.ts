a3f12c603aeabe7f89c79cf6e5ae551dac21345e
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-06-24 21:26:29 +0300

    squash!
    Signed-off-by: Nikola Hristov <Nikola@PlayForm.Cloud>

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,22 @@
 const Mobile = !!/android|ios/.exec(process.env.TAURI_ENV_PLATFORM);
 
-export default (await import("vite")).defineConfig(async () => ({
+export default (await import("vite")).defineConfig({
 	publicDir: "Public",
 	plugins: [(await import("vite-plugin-solid")).default()],
 	clearScreen: false,
 	server: {
-		port: 1420,
+		port: 9999,
 		strictPort: true,
 		host: Mobile ? "0.0.0.0" : false,
-		hmr: Mobile
-			? {
-					protocol: "ws",
-					host: await (await import("internal-ip")).internalIpV4(),
-					port: 1421,
-				}
-			: undefined,
+		hmr: {
+			protocol: "ws",
+			host:
+				(await (await import("internal-ip")).internalIpV4()) ??
+				"0.0.0.0",
+			port: 9998,
+		},
 		watch: {
 			ignored: ["**/src-tauri/**"],
 		},
 	},
-}));
+});
