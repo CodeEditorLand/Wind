ef326a06d432cbdb5b6bb681eddfb48cfc8bf130
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-06-24 21:10:43 +0300

    squash!
    Signed-off-by: Nikola Hristov <Nikola@PlayForm.Cloud>

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,11 @@ import { internalIpV4 } from "internal-ip";
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
 
-// @ts-expect-error process is a nodejs global
 const mobile = !!/android|ios/.exec(process.env.TAURI_ENV_PLATFORM);
 
-// https://vitejs.dev/config/
 export default defineConfig(async () => ({
 	plugins: [solid()],
-
-	// Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-	//
-	// 1. prevent vite from obscuring rust errors
 	clearScreen: false,
-	// 2. tauri expects a fixed port, fail if that port is not available
 	server: {
 		port: 1420,
 		strictPort: true,
@@ -26,7 +19,6 @@ export default defineConfig(async () => ({
 				}
 			: undefined,
 		watch: {
-			// 3. tell vite to ignore watching `src-tauri`
 			ignored: ["**/src-tauri/**"],
 		},
 	},
