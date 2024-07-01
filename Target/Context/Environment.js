c51320de5f828f28104bba641a1aec2521e51757
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-07-01 19:38:57 +0300

    squash!

--- Target/Context/Environment.js
+++ Target/Context/Environment.js
@@ -1,1 +1,2 @@
 import o,{_Function as t}from"./Environment/Context.js";var i=({children:r,Data:e})=>o.Data[1](e)&&React.createElement(t.Provider,{value:t.defaultValue},r);export{i as default};
+t};
