2dee8eabdb450d9f3e73932999259769e78abd5a
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-07-09 00:56:28 +0300

    squash!
    Signed-off-by: Nikola Hristov <Nikola@PlayForm.Cloud>

--- Target/Element/Link.js
+++ Target/Element/Link.js
@@ -1,1 +1,1 @@
-import{For as s,Show as i}from"solid-js";var m=({Of:r,rel:t,type:o,crossorigin:n})=>h(s,{each:r},e=>h(i,{when:e},e&&h("link",{type:o??"text/css",rel:t??"preconnect",crossorigin:n??"anonymous",href:e})));export{m as default};
+import{For as s,Show as i}from"solid-js";var m=({Of:r,rel:t,type:o,crossorigin:n})=>createComponent(s,{each:r},e=>createComponent(i,{when:e},e&&createComponent("link",{type:o??"text/css",rel:t??"preconnect",crossorigin:n??"anonymous",href:e})));export{m as default};
