5a368e1bcedc05ed9dba9417a051f82d84616ddc
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-07-08 22:28:12 +0300

    squash!

--- Target/Element/Tip.d.ts
+++ Target/Element/Tip.d.ts
@@ -13,5 +13,8 @@ export type Property = {
     onHidden?: (Instance: Instance) => void;
     Class?: string | (() => string);
 };
-declare const _default: (Property: Property) => Promise<import("solid-js").JSX.Element>;
+declare const _default: (Property: Property) => import("solid-js").JSX.Element;
 export default _default;
+export declare const Merge: <Ts extends readonly unknown[]>(...objects: Ts) => import("deepmerge-ts").DeepMergeHKT<Ts, import("deepmerge-ts").GetDeepMergeFunctionsURIs<{}>, import("deepmerge-ts").DeepMergeBuiltInMetaData>;
+export declare const Tippy: import("tippy.js").Tippy<import("tippy.js").Props>;
+export declare const createEffect: typeof import("solid-js").createEffect, on: typeof import("solid-js").on, children: typeof import("solid-js").children, SonMount: typeof import("solid-js").onMount;
