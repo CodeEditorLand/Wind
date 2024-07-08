a591e32c786cc0b92b06d9597e10b54649a5eeb1
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-07-09 00:23:27 +0300

    squash!
    Signed-off-by: Nikola Hristov <Nikola@PlayForm.Cloud>

--- Target/Context/Environment.js
+++ Target/Context/Environment.js
@@ -1,1 +1,1 @@
-import o,{_Function as t}from"./Environment/Context.js";var i=({children:r,Data:e})=>o.Data[1](e)&&hydrate(t.Provider,{value:t.defaultValue},r);export{i as default};
+import o,{_Function as t}from"./Environment/Context.js";var i=({children:r,Data:e})=>o.Data[1](e)&&h(t.Provider,{value:t.defaultValue},r);export{i as default};
