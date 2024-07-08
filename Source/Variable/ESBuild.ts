82765a84f0914f555746b867bfc5c347ea38eacb
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-07-09 02:07:39 +0300

    squash!
    Signed-off-by: Nikola Hristov <Nikola@PlayForm.Cloud>

--- Source/Variable/ESBuild.ts
+++ Source/Variable/ESBuild.ts
@@ -46,12 +46,7 @@ export default {
 				},
 			],
 		}),
-		(await import("esbuild-plugin-solid")).solidPlugin({
-			solid: {
-				hydratable: true,
-				builtIns: [],
-			},
-		}),
+		(await import("esbuild-plugin-solid")).solidPlugin(),
 	],
 } satisfies BuildOptions as BuildOptions;
 
