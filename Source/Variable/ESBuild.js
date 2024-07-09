afa475a475950d386c5935b93dbb8f9da4279d87
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-07-09 15:16:11 +0300

    squash!

--- Source/Variable/ESBuild.js
+++ Source/Variable/ESBuild.js
@@ -3,47 +3,48 @@
  *
  */
 export default {
-    color: true,
-    format: "esm",
-    logLevel: "debug",
-    metafile: true,
-    minify: true,
-    outdir: "Target",
-    platform: "node",
-    target: "esnext",
-    tsconfig: "tsconfig.json",
-    write: true,
-    plugins: [
-        {
-            name: "Target",
-            setup({ onStart, initialOptions: { outdir } }) {
-                onStart(async () => {
-                    try {
-                        outdir
-                            ? await (await import("node:fs/promises")).rm(outdir, {
-                                recursive: true,
-                            })
-                            : {};
-                    }
-                    catch (_Error) {
-                        console.log(_Error);
-                    }
-                });
-            },
-        },
-        (await import("esbuild-plugin-copy")).copy({
-            resolveFrom: "out",
-            assets: [
-                {
-                    from: ["./Source/Script/Monaco/Theme/*.json"],
-                    to: ["./Script/Monaco/Theme/"],
-                },
-                {
-                    from: ["./Source/Stylesheet/**/*.scss"],
-                    to: ["./Stylesheet/"],
-                },
-            ],
-        }),
-        (await import("esbuild-plugin-solid")).solidPlugin(),
-    ],
+	color: true,
+	format: "esm",
+	logLevel: "debug",
+	metafile: true,
+	minify: true,
+	outdir: "Target",
+	platform: "node",
+	target: "esnext",
+	tsconfig: "tsconfig.json",
+	write: true,
+	plugins: [
+		{
+			name: "Target",
+			setup({ onStart, initialOptions: { outdir } }) {
+				onStart(async () => {
+					try {
+						outdir
+							? await (
+									await import("node:fs/promises")
+								).rm(outdir, {
+									recursive: true,
+								})
+							: {};
+					} catch (_Error) {
+						console.log(_Error);
+					}
+				});
+			},
+		},
+		(await import("esbuild-plugin-copy")).copy({
+			resolveFrom: "out",
+			assets: [
+				{
+					from: ["./Source/Script/Monaco/Theme/*.json"],
+					to: ["./Script/Monaco/Theme/"],
+				},
+				{
+					from: ["./Source/Stylesheet/**/*.scss"],
+					to: ["./Stylesheet/"],
+				},
+			],
+		}),
+		(await import("esbuild-plugin-solid")).solidPlugin(),
+	],
 };
