e5987ac93ab36383e9e47938c63d8cb504d65b96
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-07-10 13:17:55 +0300

    squash!

--- Target/Context/Action/Context.js
+++ Target/Context/Action/Context.js
@@ -1,1 +1,1 @@
-const t=(await import("solid-js")).createContext({Editors:(await import("@Context/Action/Editors")).default});var o=(await import("solid-js")).useContext(t);export{t as _Function,o as default};
+const t=(await import("solid-js")).createContext({Editors:(await import("../Action/Editors.js")).default});var o=(await import("solid-js")).useContext(t);export{t as _Function,o as default};
