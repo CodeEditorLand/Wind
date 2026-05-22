f42656c0d8d16f4d124e5c6fdd68cfe175d345ff
Nikola Hristov <Nikola@PlayForm.Cloud>
2026-05-22 10:48:24 +0300

    style(Wind): Reformat ESBuild config to tab indentation
    Convert all ESBuild configuration modules under Configuration/ESBuild/ and Source/ESBuild.js from 2-space to tab indentation. Import ordering is adjusted to place local modules before external ones, and export blocks are consolidated to inline style across BaseConfig, CompileConfig, TargetConfig, BoundConstant, EnvironmentConstant, PathConstant, Target.js, Wind.js, index.js, and Source/ESBuild.js.
    
    No functional changes to the build pipeline.

--- Configuration/ESBuild/index.js
+++ Configuration/ESBuild/index.js
@@ -1,13 +1,15 @@
-export * from "./Constant/EnvironmentConstant.js";
-import { sep, posix } from "./Constant/PathConstant.js";
 import { default as default2 } from "./Config/BaseConfig.js";
-import { default as default3 } from "./Config/TargetConfig.js";
 import { default as default4 } from "./Config/CompileConfig.js";
+import { default as default3 } from "./Config/TargetConfig.js";
+import { posix, sep } from "./Constant/PathConstant.js";
+
+export * from "./Constant/EnvironmentConstant.js";
+
 export {
-  default2 as BaseConfig,
-  default4 as CompileConfig,
-  default3 as TargetConfig,
-  posix,
-  sep
+	default2 as BaseConfig,
+	default4 as CompileConfig,
+	default3 as TargetConfig,
+	posix,
+	sep,
 };
 //# sourceMappingURL=index.js.map
